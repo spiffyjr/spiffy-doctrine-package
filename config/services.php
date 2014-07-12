11f3c04eef6dff534ff9d201333ce397a2eedeb4
Kyle Spraggs <theman@spiffyjr.me>
2014-07-12 14:02:04 -0500

    Updated default filesystem cache path

--- config/services.php
+++ config/services.php
@@ -3,6 +3,6 @@
 return [
     // cache
     'doctrine.cache.array' => ['Doctrine\Common\Cache\ArrayCache'],
-    'doctrine.cache.file' => ['Doctrine\Common\Cache\FileCache', ['cache/doctrine']],
-    'doctrine.cache.filesystem' => ['Doctrine\Common\Cache\FilesystemCache', ['cache/doctrine']]
+    'doctrine.cache.file' => ['Doctrine\Common\Cache\FileCache', ['cache/doctrine/file']],
+    'doctrine.cache.filesystem' => ['Doctrine\Common\Cache\FilesystemCache', ['cache/doctrine/filesystem']]
 ];
